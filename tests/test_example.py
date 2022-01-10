5a6c5ec5a56360e132593dfb33f539c3043d3322
Luca Moschella <luca.moschella94@gmail.com>
2022-01-11 00:34:09 +0100

    Add test on conda dependency

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,14 +1,5 @@
 import pytest
 
 
-def test_add():
-    assert 4 + 8 == 12
-
-def test_subtract():
-    assert 3 - 6 == -3
-
-def test_multiply():
-    assert 4 * 5 == 20
-
-def test_divide():
-    assert 56 / 8 == 7
+def test_conda():
+    import pandas
