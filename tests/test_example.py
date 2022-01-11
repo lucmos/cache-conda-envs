8ba5aca681521b8890e5c416031137c5a04a3187
Luca Moschella <luca.moschella94@gmail.com>
2022-01-11 03:07:14 +0100

    Testing CI with conda/pip cache (#3)
    * Update environment patching
    
    * Update CI
    
    * Remove pip env update
    
    * update tests
    
    * Restore pytest conda dep
    
    * Move to ubuntu latest
    
    * Update cache number
    
    * Comment out most of the steps
    
    * Comment out most of the steps
    
    * Do not use the python version
    
    * Update cache number
    
    * Do not pip install the package
    
    * Change to conda forge
    
    * Parametrize conda env name and prefix by python version
    
    * Update cache number
    
    * Enable python version in mamba action
    
    * Fix typo

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,14 +1,7 @@
 import pytest
 
+def test_import_conda_dependency():
+    import altair
 
-def test_add():
-    assert 4 + 8 == 12
-
-def test_subtract():
-    assert 3 - 6 == -3
-
-def test_multiply():
-    assert 4 * 5 == 20
-
-def test_divide():
-    assert 56 / 8 == 7
+def test_import_pip_dependency():
+    import pandas
